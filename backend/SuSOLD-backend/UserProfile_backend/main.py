adaa0fa0b69c72471c5b634c0567f9a3beb268f3
Simay Karakış <simay.karakis@sabanciuniv.edu>
2025-04-20 16:47:12 +0300

    Main.py is created, database is connected, and required models are formed for user-profile part.

---
+++ backend/SuSOLD-backend/UserProfile_backend/main.py
@@ -0,0 +1,19 @@
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+from main_routes import main_router
+from auth_routes import auth_router
+
+app = FastAPI()
+
+origins = ['https://localhost:3000']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
+
+app.include_router(main_router)
+app.include_router(auth_router)
